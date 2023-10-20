635f3c3802fd467ea16c99205c6a49fdb8488c23
K3yJey <kevin_madrid10@hotmail.es>
2023-10-20 15:03:47 -0500

    feat: :construction: Se creó el componente SplashScreen

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,23 @@
 <template>
-  <HomeScreen />
+  <SplashScreen />
 </template>
 
 <script setup>
-import HomeScreen from "@/components/HomeScreen.vue"
+//import HomeScreen from "@/components/HomeScreen.vue"
+import SplashScreen from "@/components/SplashScreen.vue"
 </script>
 
-<style></style>
+<style>
+* {
+  --brand-green: #04b500;
+  --brand-blue: #0689b0;
+}
+
+html,
+body,
+.app {
+  min-height: 100vh;
+  margin: 0;
+  font-family: Arial, Helvetica, sans-serif;
+}
+</style>
